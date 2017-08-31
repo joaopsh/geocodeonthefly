f6cb849f70f9359adef720f755290b4d55cdbb84
Joao Pedro Souza Homem <jpsouza@stone.com.br>
2017-08-31 18:21:04 -0300

    Structured infrastructure folders to use in the future.

---
+++ Geocodeonthefly/Infrastructure/Interfaces/IAddressRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Geocodeonthefly.Domain;
+
+namespace Geocodeonthefly.Infrastructure.Interfaces
+{
+    public interface IAddressRepository
+    {
+        ICollection<Address> Read();
+
+        void Write(ICollection<Address> addresses);
+    }
+}
